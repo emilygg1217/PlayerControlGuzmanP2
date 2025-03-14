109f5e2b2fe1721b666c4a13c265943d3d78ca48
emilygg1217 <emilyg1217@icloud.com>
2025-03-14 10:08:39 -0700

    s
    s

--- GuzmanP2PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ GuzmanP2PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
     // Update is called once per frame
--- GuzmanP2PlayerControl/Assets/Scripts/PlayerController.cs
+++ GuzmanP2PlayerControl/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    public float speed = 20;
+    
+
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * 20 * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
 }
