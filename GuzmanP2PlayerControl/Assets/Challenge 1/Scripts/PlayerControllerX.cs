d92662fef08bad6066ca0623604f9756c88c84a4
emilygg1217 <emilyg1217@icloud.com>
2025-03-18 09:58:17 -0700

    h
    k

--- GuzmanP2PlayerControl/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ GuzmanP2PlayerControl/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,11 +7,12 @@ public class PlayerControllerX : MonoBehaviour
     private float speed = 20f;
     public float rotationSpeed;
     public float verticalInput;
-
+    public GameObject plane;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - plane.transform.position;
     }
 
     // Update is called once per frame
